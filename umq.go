698fc77aab61febd283904d03029d37f38601c7f
hulumeijiu <hulumeijiu@163.com>
2024-12-06 14:15:29 +0800

    2024-12-06

--- umq.go
+++ umq.go
@@ -7,13 +7,15 @@ import (
 )
 
 type Options struct {
-	Client                mqtt.Client
-	Broker                string
-	ClientID              string
-	UserName              string
-	PassWord              string
-	DefaultPublishHandler mqtt.MessageHandler
-	OnConn                mqtt.OnConnectHandler
+	Client           mqtt.Client
+	Broker           string
+	ClientID         string
+	UserName         string
+	PassWord         string
+	DefaultPublish   mqtt.MessageHandler
+	OnConn           mqtt.OnConnectHandler
+	OnReconnect      mqtt.ReconnectHandler
+	OnConnectionLost mqtt.ConnectionLostHandler
 }
 
 func (t *Options) Connect() error {
@@ -23,11 +25,21 @@ func (t *Options) Connect() error {
 	ops.SetKeepAlive(60 * time.Second)
 	ops.SetPingTimeout(5 * time.Second)
 	ops.CleanSession = true
-	ops.SetDefaultPublishHandler(t.DefaultPublishHandler)
+	ops.SetDefaultPublishHandler(t.DefaultPublish)
 	ops.SetOnConnectHandler(t.OnConn)
+	ops.SetReconnectingHandler(t.OnReconnect)
+	ops.SetConnectionLostHandler(t.OnConnectionLost)
+
 	t.Client = mqtt.NewClient(ops)
 	if token := t.Client.Connect(); token.Wait() && token.Error() != nil {
 		return token.Error()
 	}
 	return nil
+}
+
+func (t *Options) Close() {
+	if t.Client != nil {
+		t.Client.Disconnect(250)
+		time.Sleep(1 * time.Second)
+	}
 }
