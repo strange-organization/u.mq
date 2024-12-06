60b47ca3c979d3d01fded212f061b6be194569eb
hulumeijiu <hulumeijiu@163.com>
2024-12-06 10:20:50 +0800

    2024-12-06 10:20

---
+++ readme.txt
@@ -0,0 +1,7 @@
+package umq
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("Welcome, this seems to be an empty library")
+}
--- umq.go
+++ umq.go
@@ -1,7 +1,33 @@
 package umq
 
-import "fmt"
+import (
+	"time"
 
-func Hello() {
-	fmt.Println("Welcome, this seems to be an empty library")
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+type Options struct {
+	Client                mqtt.Client
+	Broker                string
+	ClientID              string
+	UserName              string
+	PassWord              string
+	DefaultPublishHandler mqtt.MessageHandler
+	OnConn                mqtt.OnConnectHandler
+}
+
+func (t *Options) Connect() error {
+	ops := mqtt.NewClientOptions().AddBroker(t.Broker).SetClientID(t.ClientID)
+	ops.SetUsername(t.UserName)
+	ops.SetPassword(t.PassWord)
+	ops.SetKeepAlive(60 * time.Second)
+	ops.SetPingTimeout(5 * time.Second)
+	ops.CleanSession = true
+	ops.SetDefaultPublishHandler(t.DefaultPublishHandler)
+	ops.SetOnConnectHandler(t.OnConn)
+	t.Client = mqtt.NewClient(ops)
+	if token := t.Client.Connect(); token.Wait() && token.Error() != nil {
+		return token.Error()
+	}
+	return nil
 }
